649ad96eb16f8fd04aee8b12d7f67753786b7c5f
Steven Nemetz <steven.nemetz@fireeye.com>
2015-06-29 13:23:03 -0700

    Remove params_lookup

--- manifests/list.pp
+++ manifests/list.pp
@@ -3,7 +3,8 @@
 # Realize a file entry or list
 #
 define files::list {
-  $list = params_lookup("files_list_${name}", 'global')
+  $list = hiera("files_list_${name}", '')
+  #$list = params_lookup("files_list_${name}", 'global')
   #$list = params_lookup("list_${name}")
   if $list != '' {
     files::list { $list:; }
